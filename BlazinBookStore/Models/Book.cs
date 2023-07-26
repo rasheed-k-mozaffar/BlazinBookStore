8772e5ecd146656763c2bd7430be470757faddaf
Rasheed Mozaffar <rasheedkmozaffar@hotmail.com>
2023-07-26 18:20:29 +0300

    Created a basic book card component to show simple book info

---
+++ BlazinBookStore/Models/Book.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace BlazinBookStore.Models
+{
+	public class Book
+	{
+		public string? Title { get; set; }
+		public string? AuthorName { get; set; }
+		public DateTime PublishingDate { get; set; }
+	}
+}
+
